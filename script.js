f10d4bb0ae41821bc89cc1e38e19e271ac00e64f
maksimgm <maksimmamrikov@gmail.com>
2015-08-17 13:39:16 -0700

    add script

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-// // Your JS goes here
-
-// var newBox = document.createElement('div');
-// document.getElementsByTagName('body')[0].appendChild(newBox);
-
-// newBox.style.backgroundColor = "red";
-// newBox.style.width = "11.1%";
-// newBox.style.float = "left";
-// newBox.style.paddingBottom = "11.1%";
-
 
 
 for(i=0; i < 63; i++){
   var newBox = document.createElement('div');
   document.getElementsByTagName('body')[0].appendChild(newBox);
-  newBox.style.backgroundColor = "red";
+  // newBox.style.backgroundColor = "red";
   newBox.style.width = "11.1%";
   newBox.style.float = "left";
   newBox.style.paddingBottom = "11.1%";
 
-  if (i % 2 === 0) {
-    newBox.style.backgroundColor = "black";
-  };
+  var r = Math.floor(Math.random()* 256);
+  var g = Math.floor(Math.random()* 256);
+  var b = Math.floor(Math.random()* 256);
+  var randomColor = (r + "," + g + ","+ b);
+  newBox.style.backgroundColor =  "r"+"g"+"b"+ "(" + randomColor+ ")";
 }
 
 
