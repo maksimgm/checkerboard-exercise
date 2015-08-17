a79a0ed50723b456c0196e3fc398f3e6e940b691
maksimgm <maksimmamrikov@gmail.com>
2015-08-17 11:49:33 -0700

    add script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your JS goes here
+// // Your JS goes here
+
+// var newBox = document.createElement('div');
+// document.getElementsByTagName('body')[0].appendChild(newBox);
+
+// newBox.style.backgroundColor = "red";
+// newBox.style.width = "11.1%";
+// newBox.style.float = "left";
+// newBox.style.paddingBottom = "11.1%";
+
+
+
+for(i=0; i < 63; i++){
+  var newBox = document.createElement('div');
+  document.getElementsByTagName('body')[0].appendChild(newBox);
+  newBox.style.backgroundColor = "red";
+  newBox.style.width = "11.1%";
+  newBox.style.float = "left";
+  newBox.style.paddingBottom = "11.1%";
+
+  if (i % 2 === 0) {
+    newBox.style.backgroundColor = "black";
+  };
+}
+
+
