133ec66ff50d98399523d9d3ed1333adbcadd280
maksimgm <maksimmamrikov@gmail.com>
2015-08-17 15:51:02 -0700

    add script

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-// // Your JS goes here
-
-// var newBox = document.createElement('div');
-// document.getElementsByTagName('body')[0].appendChild(newBox);
-
-// newBox.style.backgroundColor = "red";
-// newBox.style.width = "11.1%";
-// newBox.style.float = "left";
-// newBox.style.paddingBottom = "11.1%";
-
-var bigDiv = document.createElement('div');
-document.getElementsByTagName('body')[0].appendChild(bigDiv);
-bigDiv.style.width = "100%";
-bigDiv.style.paddingBottom = "76%";
-bigDiv.style.background = "linear-gradient(to left top, magenta, rgba(30,90,40,.04))";
-bigDiv.style.position= "absolute";
-
-
 for(i=0; i < 63; i++){
-  
   var newBox = document.createElement('div');
   document.getElementsByTagName('body')[0].appendChild(newBox);
-  newBox.style.backgroundColor = "red";
+  // newBox.style.backgroundColor = "red";
   newBox.style.width = "11.1%";
   newBox.style.float = "left";
   newBox.style.paddingBottom = "11.1%";
 
-  // use linear gradient
-    if (i % 2 === 0) {
-    newBox.style.backgroundColor = "yellow";
-  };
+  var r = Math.floor(Math.random()* 256);
+  var g = Math.floor(Math.random()* 256);
+  var b = Math.floor(Math.random()* 256);
+  var randomColor = (r + "," + g + ","+ b);
+  newBox.style.backgroundColor =  "r"+"g"+"b"+ "(" + randomColor+ ")";
 }
+
+
+  var intervalId = window.setInterval(function() {
+  // for loop 
+  // change BG color of every div
+for(i=0; i < 63; i++){
+  var newerBox = document.getElementsByTagName('div')[i];
+  var r = Math.floor(Math.random()* 256);
+  var g = Math.floor(Math.random()* 256);
+  var b = Math.floor(Math.random()* 256);
+  var randomColor = (r + "," + g + ","+ b);
+  newerBox.style.backgroundColor =  "r"+"g"+"b"+ "(" + randomColor+ ")";
+  }
+},2000);
+
