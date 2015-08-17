a274537a1a1778cc5d4128ee159517556ae9e1c8
maksimgm <maksimmamrikov@gmail.com>
2015-08-17 14:37:08 -0700

    add gradient

--- script.js
+++ script.js
@@ -8,9 +8,16 @@
 // newBox.style.float = "left";
 // newBox.style.paddingBottom = "11.1%";
 
+var bigDiv = document.createElement('div');
+document.getElementsByTagName('body')[0].appendChild(bigDiv);
+bigDiv.style.width = "100%";
+bigDiv.style.paddingBottom = "76%";
+bigDiv.style.background = "linear-gradient(to left top, magenta, rgba(30,90,40,.04))";
+bigDiv.style.position= "absolute";
 
 
 for(i=0; i < 63; i++){
+  
   var newBox = document.createElement('div');
   document.getElementsByTagName('body')[0].appendChild(newBox);
   newBox.style.backgroundColor = "red";
@@ -18,9 +25,8 @@ for(i=0; i < 63; i++){
   newBox.style.float = "left";
   newBox.style.paddingBottom = "11.1%";
 
-  if (i % 2 === 0) {
-    newBox.style.backgroundColor = "black";
+  // use linear gradient
+    if (i % 2 === 0) {
+    newBox.style.backgroundColor = "yellow";
   };
 }
-
-
